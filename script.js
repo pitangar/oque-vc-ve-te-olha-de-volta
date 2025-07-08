47c3908bebfc04be0cb1ecfd5ffa6c34dbc6c4d1
carolina <carolochotorena@gmail.com>
2025-07-07 22:08:01 -0300

    Links novos instagram

--- script.js
+++ script.js
@@ -12,4 +12,24 @@ document.addEventListener('DOMContentLoaded', () => {
   dropdown.addEventListener('mouseleave', () => {
     dropdownContent.style.display = 'none';
   });
+
+  // Aqui o código do container:
+  const container = document.createDocumentFragment();
+  for(let i = 3; i <= 15; i++) {
+    const bloco = document.createElement('section');
+    bloco.className = `exercise ${i % 2 === 1 ? 'bloco1' : 'bloco2'}`;
+    bloco.id = `desafio${i}`;
+
+    bloco.innerHTML = `
+      <div class="container-desafio">
+        <div class="titulo-lado-esquerdo">
+          <h2>DESAFIO ${i}</h2>
+        </div>
+        <div class="texto-lado-direito">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proposta do desafio ${i}.</p>
+        </div>
+      </div>`;
+    container.appendChild(bloco);
+  }
+  document.body.insertBefore(container, document.getElementById('footer'));
 });
