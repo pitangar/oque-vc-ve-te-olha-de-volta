075dc56dd9dddf2911ccaa3a49412ebb38d8eeea
carolina <carolochotorena@gmail.com>
2025-07-07 22:52:21 -0300

    resolução nav bar

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const dropdown = document.querySelector('.dropdown');
-  const dropdownContent = dropdown.querySelector('.dropdown-content');
+  const dropdowns = document.querySelectorAll('.dropdown');
 
-  dropdownContent.style.display = 'none'; // Esconde ao carregar
-
-  dropdown.addEventListener('mouseenter', () => {
-    dropdownContent.style.display = 'block';
-  });
-
-  dropdown.addEventListener('mouseleave', () => {
+  dropdowns.forEach(dropdown => {
+    const dropdownContent = dropdown.querySelector('.dropdown-content');
     dropdownContent.style.display = 'none';
+
+    dropdown.addEventListener('mouseenter', () => {
+      dropdownContent.style.display = 'block';
+    });
+
+    dropdown.addEventListener('mouseleave', () => {
+      dropdownContent.style.display = 'none';
+    });
   });
 });
